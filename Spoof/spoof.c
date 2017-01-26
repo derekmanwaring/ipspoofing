4bfe81664c988eabc2ce9e4df6c540f1805a7a94
Derek Manwaring <derekmn@uw.edu>
2017-01-26 15:32:09 -0800

    Got ICMP unreachable response

--- Spoof/spoof.c
+++ Spoof/spoof.c
@@ -11,69 +11,82 @@
 
 #include <unistd.h>
 
-int sockoptval = 1;
-
-int main(int argc, char **argv) {
+int send_ip_datagram(const const char* source_address,
+        const const char* dest_address,
+        const const void* payload,
+        size_t payload_length,
+        uint8_t protocol) {
+    int socket_desc = 0;
+    char datagram[2048];
+    uint16_t ip_datagram_length;
     struct sockaddr_in destination;
+    struct iphdr *iph;
+    int retv; // scratch for return values
 
-    int fd = 0;
-    fd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    // clear datagram buffer
+    memset(datagram, 0, 2048);
 
-    if (fd == -1) {
+    // set up socket
+    socket_desc = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if (socket_desc == -1) {
         perror("Could not create socket");
         return -1;
     }
-
-    char datagram[4096];
-    char *data;
-
-    memset(datagram, 0, 4096);
-
-    struct iphdr *iph = (struct iphdr *) datagram;
-
-    destination.sin_family = AF_INET;
-    destination.sin_port = htons(39640);
-    destination.sin_addr.s_addr = htons(1);
-
-    destination.sin_family = AF_INET;
-    destination.sin_port = htons(80);
-    // www.google.com
-    destination.sin_addr.s_addr = inet_addr("172.217.3.164");
-
-    data = datagram + sizeof (struct iphdr) + sizeof (struct tcphdr);
-    const const char* payload = "payload";
-    strncpy(data, payload, sizeof (datagram) - sizeof (struct iphdr) - sizeof (struct tcphdr) - 1);
-
-    iph->ihl = 5;
-    iph->version = 4;
-    iph->tos = 0;
-    iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr) + strlen(payload);
-    iph->id = htonl(54321);
-    iph->frag_off = 0;
-    iph->ttl = 255;
-    iph->protocol = IPPROTO_TCP;
-    iph->check = 0;
-    iph->saddr = htons(1);
-    iph->daddr = destination.sin_addr.s_addr;
-
-    int retv;
-    retv = setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &sockoptval, sizeof (int));
+    int enable_headers_included = 1;
+    retv = setsockopt(socket_desc, IPPROTO_IP, IP_HDRINCL,
+            &enable_headers_included,
+            sizeof (enable_headers_included));
     if (retv != 0) {
         perror("Couldn't set headers included option");
         return -1;
     }
 
-    retv = sendto(fd, datagram, iph->tot_len, 0, (struct sockaddr *) &destination, sizeof (destination));
-    if (retv != iph->tot_len) {
+    // set destination address and ip header
+    ip_datagram_length = sizeof (struct iphdr) + payload_length;
+    destination.sin_family = AF_INET;
+    destination.sin_addr.s_addr = inet_addr(dest_address);
+    iph = (struct iphdr *) datagram;
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = htons(ip_datagram_length);
+    iph->id = 0;
+    iph->frag_off = 0;
+    iph->ttl = 255;
+    iph->protocol = protocol;
+    iph->check = 0;
+    iph->saddr = inet_addr(source_address);
+    iph->daddr = destination.sin_addr.s_addr;
+
+    // copy the payload into buffer after header
+    memcpy(datagram + sizeof(struct iphdr), payload, payload_length);
+
+    // send the datagram
+    retv = sendto(socket_desc, datagram, ip_datagram_length, 0,
+            (struct sockaddr *) &destination, sizeof (destination));
+    if (retv != ip_datagram_length) {
         perror("Couldn't send all data");
         return -1;
     }
 
-    retv = close(fd);
+    // cleanup
+    retv = close(socket_desc);
     if (retv != 0) {
         perror("Error closing socket");
         return -1;
     }
 
     return 0;
+
+}
+
+int main(int argc, char **argv) {
+    const const char* source_address = argv[1];
+    const const char* dest_address = argv[2];
+    const const void* payload = "payload";
+    size_t payload_length = strlen(payload);
+    uint8_t protocol = IPPROTO_IP;
+
+    return send_ip_datagram(source_address, dest_address,
+            payload, payload_length, protocol);
 }
